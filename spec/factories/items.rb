328816050a6b7ba8122fdabde2c9ebf87437dc98
Sidmar Theodoro <theodorosidmar@gmail.com>
2018-10-23 17:37:27 -0300

    [REFACTOR] Factory usage

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@
 FactoryBot.define do
   factory :item do
     name { Faker::StarWars.character }
-    done false
-    todo_id nil
+    done { false }
+    todo_id { nil }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@
 FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
-    email 'foo@bar.com'
-    password 'foobar'
+    email { 'foo@bar.com' }
+    password { 'foobar' }
   end
 end
